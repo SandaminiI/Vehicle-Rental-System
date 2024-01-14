405c2c811e18964dbe9f195edcf620031f2e9e67
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2024-01-14 17:23:02 +0530

    Create About us.php

---
+++ About us.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>About Us</title>
+	
+	<!-- link CSS styles for header and footer-->
+    <link rel="stylesheet" href="Styles/styles.css">
+	
+	<!--link stylesheet for body-->
+    <link rel="stylesheet" href="Styles/about us.css">
+	
+	<!--footer stylesheet icons-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+    <!--font-awsome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head> 
+
+<body>
+
+<!--header-->
+<!-- add a horizontal menu bar --> 
+                <button type="button" class="rentbtn"><a href="reservation.php">Rent a vehicle</a></button>
+                <div class="navbar">
+                    
+                    <a href="home page.html">Home</a>
+                    <a href="login.php">Login</a>
+                    <div class="servicebtn">
+                        <button class="dropbtn">Service
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="servicebutton-content">
+                            <a href="services.php">Select vehicle</a>
+                        </div>
+                        
+                    </div>
+        
+                    <div class="profilesbtn">
+                        <button class="dropbtn">Profiles
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="profilesbutton-content">
+                            <a href="register.php">Create an account</a>
+                            <a href="user.php">User account</a>
+                            <a href="Vehicle_owner.php">Vehicle owner account</a>
+                            <a href="adminpage.php">Admin account</a>
+                        </div>
+                    </div>
+        
+                    <div class="aboutusbtn">
+                        <button class="dropbtn">About us
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="aboutusbutton-content">
+                            <a href="Terms and conditions.php">Terms & Conditions</a>
+                            <a href="Privacy & Policy page.html">Privacy & Policy</a>
+                            </div>
+                        </div>
+                    <a href="contactUs.html">Contact Us</a>
+    </div>
+	
+    <!--content-->
+	
+	<!-- About Start -->
+	
+	<!-- Title -->
+	<br><br>
+    <h1><i>TravelLine Vehicle Rental</i></h1>
+	<br>
+	
+	
+    <div class="heading">
+        <h1><u>About Us</u></h1>
+        <p>Welcome to our vehicle rental company! We are a leading provider of reliable and convenient transportation solutions for individuals and
+		   businesses alike.With a wide range of vehicles and exceptional customer service, we strive to make your rental experience smooth, enjoyable, 
+		   and hassle-free.</p>
+    </div>
+	
+    <hr class="new1">
+    <br><br>
+
+    <!--vision,mission,target-->
+    <div class="clearfix">
+    <div class="box" style="background-color: #ffcc66">
+    <center><h2><u>Our Vision</u></h2><br><p>Our vision as a vehicle rental company is to be the preferred and leading provider of transportation
+	             solutions, setting the benchmark for exceptional service, innovation, and sustainability in the<br> industry.</p></center>
+	</div>
+	
+    <div class="box" style="background-color:#ffd480">
+    <center><h2><u>Our Mission</u></h2><br><p>Our mission at our vehicle rental company is to provide exceptional transportation solutions that 
+	             empower our customers to explore, travel, and achieve their goals with convenience, reliability, and peace of mind.</p></center>
+    </div>
+	
+    <div class="box" style="background-color:#ffdd99">
+    <center><h2><u>Our Target</u></h2><br><p>Our target is to cater to a diverse range of customers, including individuals, families, tourists,
+	              and businesses. Our aim is to provide transportation solutions that meet the specific needs and preferences of each target group.</p></center>
+    </div>
+    </div>
+   
+    <br><br>
+   
+    <!-- content-->
+    <div class="container">
+    <section class="about">
+	
+    <div class="about-content">
+    <p align="justity">At our vehicle rental company, we understand that each customer has unique needs and preferences. Whether you need a compact car 
+      for a quick city trip, a spacious SUV for a family vacation, or a sleek luxury vehicle for a special occasion, we have a diverse fleet of well-maintained 
+      vehicles to cater to your requirements.What sets us apart is our commitment to delivering exceptional customer service. Our friendly and knowledgeable 
+	  staff are always ready to assist you in finding the perfect vehicle for your needs. We provide transparent pricing, flexible rental periods, and various 
+	  rental options to accommodate your schedule.We prioritize your safety and comfort, which is why we regularly inspect and maintain our vehicles to ensure 
+	  they are in top condition. </p>
+    </div>
+    <div class="about-image">
+    <img src="Images/Brand-Scroll.jpg" width="500px" height="250px">
+    </div>
+    </section>
+    </div>
+    <br><br><br>
+
+
+    <div class="container">
+    <section class="about">
+    <div class="about-image">
+    <img src="Images/car rental.jpg" width="500px" height="250px">
+    </div>
+    <div class="about-content">
+    <p>Our fleet consists of the latest models equipped with modern safety features and technology, guaranteeing a smooth and secure driving experience.
+	  Convenience is at the core of our services. We offer easy online booking options, allowing you to reserve your desired vehicle with just a few clicks. 
+	  Additionally, we have multiple pick-up and drop-off locations, providing you with flexibility and accessibility wherever you may be.Whether you're a
+	  tourist exploring a new city, a business traveler attending meetings, or simply in need of a temporary vehicle, we have the perfect rental solution 
+      for you. Our commitment to customer satisfaction and our dedication to providing reliable and high-quality vehicles make us the preferred choice for 
+	  many.</p>
+    </div>
+	</section>
+    </div>
+    <br><br><br><br>
+
+
+    <div class="container">
+    <section class="about">
+    <div class="about-content">
+    <p>We value our customers and aim to build long-lasting relationships based on trust and exceptional service. Your feedback is important to us, 
+	  as it helps us continually improve and tailor our services to meet your evolving needs.In conclusion, our vehicle rental company is dedicated to 
+	  providing exceptional transportation solutions that meet the diverse needs of our customers. With a focus on convenience, reliability, and innovation, 
+	  we strive to create memorable experiences and exceed customer expectations.We differentiate ourselves by offering a wide range of vehicles, from compact
+	  cars to luxury vehicles, ensuring there is a perfect option for every occasion and preference. </p>
+    </div>
+    <div class="about-image">
+    <img src="Images/T707192034.jpg" width="500px" height="250px">
+    </div>
+    </section>
+    </div>
+    <br><br><br>
+
+
+    <h2><center>Thank you for choosing TravelLine vehicle rental company. We look forward to serving you and being a part of your memorable journeys.<br>
+	"Safe travels!"</center></h2>
+    <br><br><br>
+     
+	 
+	<h3><center>Make an inquiry<center></h3>
+	
+
+
+    <center>
+	</br>
+	<!--button-->
+	<a href="contactUs.html" class="btn">INQUIRE NOW</a>
+	<br><br><br><br>
+	
+	<h2>Hotline : (+94) +94 112 345 678 </h2>
+ 
+    <?php
+	echo "<p>Office Hours: Monday - Friday 0830 - 1730 (Sri Lanka Time) [+5.5 GMT]</p>
+	<p>Office: 70/5, Post office Road , Malabe.</p>
+	<br>
+	<p>Find us on Social Media below: Facebook &nbsp&nbsp&nbsp Instagram &nbsp&nbsp&nbsp  Pinterest<p>";
+	?>
+	
+	</center>
+		
+	</br></br></br>
+	
+	
+    <!-- About End -->
+	
+	
+	
+	  
+	<!--footer-->
+    <footer>
+    <div class="foot">
+        <div class="footer">
+            <div class="row">
+                <dic class="col" id="company">
+
+                    <img src="Images/logo.jpg" alt="" class="logo"><br>
+
+                    <p>"With the help of our amazing rental fleet, unleash your trip. <br> Go off exploring!"</p>
+                    
+                    <div class="social"><br>
+                        <a href="www.facebook.com"><i class="fab fa-facebook"></i></a>
+                        <a href="www.instagram.com"><i class="fab fa-instagram"></i></a>
+                        <a href="www.twitter.com"><i class="fab fa-twitter"></i></a>
+                        <a href="www.youtube.com"><i class="fab fa-youtube"></i></a>
+                    </div>
+
+                </div>
+
+                <div class="col" id="useful-links"><br>
+                    <h3>Links</h3>
+                    <div class="link">
+                        <a href="About us.php">About us</a>
+                        <a href="services.php">Services</a>
+                        <a href="Privacy & Policy page.html">Privacy and Policy</a>
+                        <a href="Terms and conditions.php">Terms and condditions</a>
+                        <a href="contactUs.html">Contact us</a>
+                    </div>
+                </div>
+
+                <div class="col" id="contacts"><br>
+                    <h3>Contact</h3>
+                    <div class="contact-details">
+                        <p>Address :70/5,  Post Office Road,  Malabe</p><br>
+                        <p><i class="material-icons">call : </i>+94-112345678</p>
+                        <p><i class="material-icons">email : </i>TravelLine.SriLanka@email.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    </footer>
+	  
+</body>
+
+</html>	  
