cba2b1c188386a9fba9152acd628ac3ee5a75bba
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2024-01-14 17:23:19 +0530

    Create Terms and conditions.php

---
+++ Terms and conditions.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Submit staff details</title>
+	
+	<!-- link CSS styles for header and footer-->
+    <link rel="stylesheet" href="Styles/styles.css">
+	
+	<!--link stylesheet for body-->
+    <link rel="stylesheet" href="Styles/terms & conditions.css">
+	
+	<!--footer stylesheet icons-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+    <!--font-awsome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+	
+	
+</head>
+
+<body>
+
+<!--header-->
+<!-- add a horizontal menu bar --> 
+                <button type="button" class="rentbtn"><a href="reservation.php">Rent a vehicle</a></button>
+                <div class="navbar">
+                    
+                    <a href="home page.html">Home</a>
+                    <a href="login.php">Login</a>
+                    <div class="servicebtn">
+                        <button class="dropbtn">Service
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="servicebutton-content">
+                            <a href="services.php">Select vehicle</a>
+                        </div>
+                        
+                    </div>
+        
+                    <div class="profilesbtn">
+                        <button class="dropbtn">Profiles
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="profilesbutton-content">
+                            <a href="register.php">Create an account</a>
+                            <a href="user.php">User account</a>
+                            <a href="Vehicle_owner.php">Vehicle owner account</a>
+                            <a href="adminpage.php">Admin account</a>
+                        </div>
+                    </div>
+        
+                    <div class="aboutusbtn">
+                        <button class="dropbtn">About us
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="aboutusbutton-content">
+                            <a href="Terms and conditions.php">Terms & Conditions</a>
+                            <a href="Privacy & Policy page.html">Privacy & Policy</a>
+                            </div>
+                        </div>
+                    <a href="contactUs.html">Contact Us</a>
+                </div>
+	  
+	<!--content-->
+	
+	<br><br><br>
+	<h1>Our Terms & Conditions</h1>
+	<br><br>
+
+	<div class="paragraph"><center>
+	Welcome to TravelLine Vehicle Rental. These Terms and Conditions ("Terms") govern your use of our website and services. By accessing or using our website,
+	you agree to be bound by these Terms. Please read them carefully.</center>
+	</div>
+	
+	<br><br>
+	
+	
+	<div class="clearfix">
+	
+    <div class="box" style="background-color: #85e085">
+        <h2>Eligibility</h2><br>
+	    <li>Users must be at least 18 years old and legally capable of entering into binding contracts.</li>
+	    <li>Users are responsible for providing accurate and up-to-date information during registration.</li>
+	</div>
+	
+    <div class="box" style="background-color:#66ffb2">
+        <h2>Rental Requirements</h2><br>
+	    <li>Users must possess a valid driver's license.</li>
+	    <li>Users must meet any additional requirements specified by the rental company, such as minimum age restrictions or additional insurance coverage.</li>
+    </div>
+	
+    <div class="box" style="background-color:#00e673">
+        <h2>Cancellations </h2><br>
+	    <li>Cancellation policies and applicable fees are specified during the reservation process.</li>
+	    <li>Users may be eligible for a refund based on the cancellation policy and the time of cancellation.</li>
+    </div>
+	
+    </div>
+	
+	<br><br><br>
+	
+	
+	<div class="clearfix">
+	
+    <div class="box" style="background-color:#00e6ac">
+        <h2>Vehicle Rental</h2><br>
+	    <li>Users can browse available vehicles, select a rental period, and make a reservation through the website.</li>
+	    <li>Rental rates, fees, and payment terms are provided on the website and may vary depending on the selected vehicle and rental period.</li>
+	    <li>Rental duration starts and ends at the specified dates and times agreed upon during the reservation.</li>
+	</div>
+	
+    <div class="box" style="background-color:#8cd98c">
+        <h2>User Obligations</h2><br>
+        <li>Users must comply with all applicable regulations.</li>
+        <li>Users must not use the website for any unlawful or unauthorized purposes.</li>
+        <li>Users are responsible for the proper use and care of the rented vehicle.</li>
+        <li>Users are liable for any damages or losses incurred during the rental period.</li>
+    </div>
+	
+    <div class="box" style="background-color:#40bf80">
+        <h2>Insurance</h2><br>
+	    <li>The website may offer insurance coverage options for rented vehicles.</li>
+	    <li>Users are responsible for reviewing and understanding the insurance terms and coverage before renting a vehicle.</li>
+	    <li>Claims and coverage disputes are handled between the user and the insurance provider directly.</li>
+    </div>
+	
+    </div>
+	
+	<br><br><br>
+	
+	
+	<br><br>
+	<div class="clearfix">
+	<div class="box" style="background-color:#00e673">
+        <h2>Governing Law</h2><br>
+	    <li>These Terms shall be governed by and construed in accordance with the laws of [Jurisdiction].</li><br>
+        <li>Any disputes arising out of these Terms shall be resolved through arbitration or the courts of [Jurisdiction].</li>
+    </div>
+	
+    <div class="box" style="background-color:#8cd98c">
+        <h2>Limitation of Liability</h2><br>
+	    <li>The website and its owners are not liable for any damages, losses, or injuries resulting from the use of the website or rental services.</li>
+        <li>Users agree to release the website and its owners from any claims, liabilities, or disputes arising from the rental.</li>
+    </div>
+	
+    <div class="box" style="background-color:#40bf80">
+        <h2>Intellectual Property</h2><br>
+	    <li>All content and materials on the website, including trademarks, logos, text, images, and software, are owned by or licensed to the website.</li>
+	    <li>Users may not use, reproduce, modify, or distribute any content from the website without prior written permission</li>
+    </div>
+	
+    </div>
+	
+	<br><br><br>
+	
+	
+	<div class="paragraph">In conclusion, we appreciate your time and attention in reviewing the terms and conditions outlined on our vehicle rental website.
+	  These terms and conditions are not merely legal jargon but an essential framework that governs the rental process, ensuring a smooth,
+	  safe, and satisfactory experience for all parties involved. By clearly defining the rights, responsibilities, and obligations of both our customers and 
+	  our company, we aim to establish a transparent and trustworthy relationship. We understand that renting a vehicle is an important decision, and we strive to 
+	  provide you with all the necessary information to make an informed choice.
+
+      Through these terms and conditions, we outline the rental procedures, including reservation, payment, and cancellation policies, to ensure 
+	  a seamless process from start to finish. We also highlight important details such as age restrictions, driver's license requirements, and 
+	  insurance coverage, to guarantee compliance with legal and safety standards. Additionally, our terms and conditions address the use of the
+	  rented vehicle, emphasizing the importance of responsible driving, adherence to traffic laws, and proper care of the vehicle. We also specify 
+	  any limitations, such as mileage restrictions or restrictions on crossing international borders, to protect both the vehicle and our customers.<br><br>
+
+      Furthermore, our terms and conditions cover liability and indemnity, making it clear who is responsible for damages, loss, or accidents during 
+	  the rental period. It is crucial for both parties to understand their respective liabilities and the steps to take in case of any unforeseen 
+	  circumstances. We have carefully crafted these terms and conditions to ensure fairness, transparency, and mutual respect. It is important that 
+	  you read and comprehend these policies before making a reservation. By doing so, you demonstrate your commitment to upholding our standards 
+	  and your understanding of the expectations set forth.We value your trust in our vehicle rental service and are committed to providing exceptional 
+	  customer experiences. Our terms and conditions serve as a foundation for open communication and dispute resolution, should any issues arise during 
+	  the rental period. We are here to assist and address any concerns you may have, and we encourage you to reach out to our dedicated customer support 
+	  team for assistance.</div>
+	
+	<br><br><br>
+
+    <?php 
+	echo "<h3><center>Thank you for choosing our vehicle rental service. We are confident that by abiding by these terms and conditions, we can establish a 
+	  mutually beneficial partnership, ensuring a positive and enjoyable rental experience. We look forward to serving you and meeting your transportation 
+	  needs, and we appreciate the opportunity to be your trusted vehicle rental provider.</center></h3>";
+	?>  
+
+
+    <br><br><br>
+	  
+	 <!--footer-->
+    <footer>
+    <div class="foot">
+        <div class="footer">
+            <div class="row">
+                <dic class="col" id="company">
+
+                    <img src="Images/logo.jpg" alt="" class="logo"><br>
+
+                    <p>"With the help of our amazing rental fleet, unleash your trip. <br> Go off exploring!"</p>
+                    
+                    <div class="social"><br>
+                        <a href="www.facebook.com"><i class="fab fa-facebook"></i></a>
+                        <a href="www.instagram.com"><i class="fab fa-instagram"></i></a>
+                        <a href="www.twitter.com"><i class="fab fa-twitter"></i></a>
+                        <a href="www.youtube.com"><i class="fab fa-youtube"></i></a>
+                    </div>
+
+                </div>
+
+                <div class="col" id="useful-links"><br>
+                    <h3>Links</h3>
+                    <div class="link">
+                        <a href="About us.php">About us</a>
+                        <a href="services.php">Services</a>
+                        <a href="Privacy & Policy page.html">Privacy and Policy</a>
+                        <a href="Terms and conditions.php">Terms and condditions</a>
+                        <a href="contactUs.html">Contact us</a>
+                    </div>
+                </div>
+
+                <div class="col" id="contacts"><br>
+                    <h3>Contact</h3>
+                    <div class="contact-details">
+                        <p>Address :70/5,  Post Office Road,  Malabe</p><br>
+                        <p><i class="material-icons">call : </i>+94-112345678</p>
+                        <p><i class="material-icons">email : </i>TravelLine.SriLanka@email.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    </footer>
+	  
+</body>
+</html>	  
